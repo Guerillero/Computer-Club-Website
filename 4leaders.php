dd727fa9e5132dec7bf9d4999ca859d558d3ef5b
Tom Fish <guerillero.wikipedia@gmail.com>
2014-11-13 22:29:46 -0500

    Create 4leaders.php

---
+++ 4leaders.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<head lang="en">
+        <meta charset="utf-8">
+
+        <title>Leader Resources</title>
+        <meta name="description" content="Resources for board members of the Computer Club.">
+
+        <link rel="stylesheet" href="./main.css">
+</head>
+
+<body>
+<?php require("./includes/header.php") ?>
+
+<section>
+        <header>
+        <h1>Leader Resources</h1>
+        </header>
+        <p>This page contains resources that are useful to the leaders of the computer club. While everyone may access them, they aren't of much use to general members.</p><p> <strong>UNDER CONSTRUCTION</strong></p>
+</section>
+
+<section>
+        <header>
+        <h1>Guides</h1>
+        </header>
+        <ul>
+                <li><a href="howToLANParty.php">How to Plan and Run a LAN Party</a></li>
+                <li><a href="howToCompleteABudgetProposal.php">How to Complete an SGA Budget Proposal</a></li>
+                <li><a href="howToGetReimbursed.php">How to Get Reimbursed for a Club Expenditure</a></li>
+        </ul>
+</section>
+<section>
+        <header>
+        <h1>SGA Forms</h1>
+        </header>
+        <ul>
+                <li><a href="files/sga/event-registration-form.pdf">Event Registration Form</a> &mdash; Necessary if the event is open to the whole campus, expects more than 20 attendees, or will offer food and/or drink.</li>
+                <li><a href="">Hodson Hall Space Reservation Form</a> &mdash; Use if reserving the following spaces: The Egg, Goose Nest, Student Lounge, Hodson Green, Hodson Lobby (or a table in the lobby), and requests for events after 5:00 p.m. in the Hodson Faculty Lounge. This includes reserving a table at the bottom of the main stairs leading to the dining hall.</li>
+                <li><a href="https://www.washcoll.edu/live/forms/336-table-rental?preview=1">SGA-owned Table Reservation Form</a> &mdash; Use if you want to reserve some of the tables that the SGA bought for club use. They bought 50. These tables are separate from the ones used at the base of the main stairs leading to the dining hall.</li>
+                <li><a href="files/sga/alcohol-event-registration-form.pdf">Alcohol Event Registration Form</a> &mdash; Necessary if alcohol will be served at the event.</li>
+                <li><a href=""></a></li>
+        </ul>
+</section>
+<?php require("./includes/footer.php") ?>
+</body>
+</html>
