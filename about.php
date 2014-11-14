4023435012af4cc473e9195d9408a8c7377ec909
Tom Fish <guerillero.wikipedia@gmail.com>
2014-11-13 22:30:41 -0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<head lang="en">
+        <meta charset="utf-8">
+
+        <title>About</title>
+        <meta name="description" content="A description of the Washington College Computer Club.">
+
+        <link rel="stylesheet" href="./main.css">
+</head>
+
+<body>
+<?php require("./includes/header.php") ?>
+
+<section>
+        <header>
+        <h1>About</h1>
+        </header>
+        <p>The Washington College Computer Club is a community of people interested in computers. Anybody is welcome to join, regardless of skill level. The only prerequisite is either a desire to learn about computers or a desire to help with a project that involves computers.</p>
+</section>
+
+<section>
+        <header>
+        <h1>Current Officers</h1>
+        </header>
+        <ul>
+                <li><strong>President</strong>&mdash;Ian Egland<br /><em>Ian resurrected the computer club after nearly ten years of dormancy. He believes that the members of the club are its owners, not its officers or advisor.</em></li>
+                <li><strong>Vice President</strong>&mdash;Brian Gicking<br /></li>
+                <li><strong>Treasurer</strong>&mdash;Bonnie Douglas<br /><em>Bonnie is a Biology major and Chemistry minor who works as a biology lab prep and a peer writing tutor. She can usually be found hunched over a lab bench or laboring under a hood, but when the goggles come off Bonnie likes to dabble in cooking and the visual arts. While her experience with computers is limited, she enjoys learning all that she can as a member of the club!</em></li>
+                <li><strong>Public Service Coordinator</strong>&mdash;Thomas Fish<br /><em>Tom is a Senior Philosophy and Anthropology double major. He works for the <a href="http://www.washcoll.edu/centers/ces/gis/">GIS Program</a> and, in his spare time, he plays rugby, volunteers for <a href="http://www.wikipedia.org"/>Wikipedia</a>, and is the lead sysop for <a href="http://washcoll.net/">washcoll.net</a>.</em></li>
+        </ul>
+</section>
+
+<section>
+        <header>
+                <h1>Faculty Advisor</h1>
+        </header>
+        <p>
+<a href="http://shaunramsey.com/">Dr. Shaun Ramsey</a>
+        </p>
+</section>
+
+
+<?php require("./includes/footer.php") ?>
+</body>
+</html>
