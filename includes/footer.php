fe0b31d40a7649626fc0c98aad1eb3843f91833a
Tom Fish <guerillero.wikipedia@gmail.com>
2014-11-13 22:49:24 -0500

    more files from ian

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer>
+        <p>
+        <a href="https://github.com/Guerillero/Computer-Club-Website"><img height="50" title="Fork me on GitHub "  src="./images/GitHub.png"></a>
+        <a href ="./license.html"><img height="50" title="Free Software" src="./images/copyleft.png"></a>
+        </p>
+        <p>
+        <a href="./4leaders.php">Leadership Resources</a>
+        </p>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<header>
+        <h1><a href="//inferno.washcoll.edu">Washington College Computer Club</a></h1>
+        <nav>
+        <ul>
+                <li><a href="./about.php">About</a></li>
+		<li><a href="./meetings.php">Meetings</a></li>
+		<li><a href="./projects.php">Projects</a></li>
+		<li><a href="http://washcoll.net/wiki/index.php/Projects">Project Ideas</a></li>
+                <li><a href="./finances.php">Finances</a></li>
+                <li><a href="./resources.php">Resources</a></li>
+		<li><a href="./wishlist.php">Wish List</a></li>
+                <li><a href="https://list.washcoll.edu/mailman/listinfo/computer_club">Mailing List</a></li>
+        </ul>
+        </nav>
+</header>
